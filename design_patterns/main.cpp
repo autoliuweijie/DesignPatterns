c0aa9984120c59fc8ec126e1c276f131fad7bcde
autoliuweijie <autoliuweijie@163.com>
2015-12-15 14:27:15 +0800

    init project

---
+++ design_patterns/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  design_patterns
+//
+//  Created by liuweijie on 15/12/15.
+//  Copyright © 2015年 dataliu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
