94cc4ff22dc821bc43a4b253f1ab1531b1253c39
autoliuweijie <autoliuweijie@163.com>
2015-12-29 21:03:23 +0800

    start decorator in python

---
+++ design_patterns/6.decorator/python/decorator.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""
+    Example of  Decorator pattern.
+    @author: Liu Weijie
+    @data: 2015-12-19
+
+    需求:
+        设计一个给发票对象添加新功能的装饰器:
+            所有发票都有打印正文的功能, 但是有些发票有表头或页脚.
+"""
+
